b28d7e9d490f88fdf1444abd7f0b285d715c2a34
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2020-03-02 12:01:41 -0600

    doesnt matter

---
+++ caughtja/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>
+            List of Events by Joshua Kuruvilla, Chris Garcia, Tedla Betsuan
+        </div>
+    )
+}
+
+export default Footer;
---
+++ caughtja/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            List of Events
+        </div>
+    )
+}
+
+export default Header;
