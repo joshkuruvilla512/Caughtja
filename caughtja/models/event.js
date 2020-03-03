5d56e038164702d466785d57a42703bd9d35a6bb
Tedla B <mekdi.td87@outlook.com>
2020-03-02 19:14:40 -0600

    resolved the console error

--- caughtja/models/event.js
+++ caughtja/models/event.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
   Event: { type: String, required: true },
-  organizer: { type: String, required: true },
+  organizer: { type: String, required: false },
   Purpose: String,
   // Address: {type: String, required: false},
   // Tel: { type: String, required: false }
