e28d60883f189fc482feacf13d4ebabc7e908180
krislg420 <57952065+krislg420@users.noreply.github.com>
2020-03-03 13:19:55 -0600

    index.js

--- caughtja/client/src/components/Nav/index.js
+++ caughtja/client/src/components/Nav/index.js
@@ -6,13 +6,18 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-info justify-content-between">
       <div>
-        {/* <h1 class="text-hide" style="background-image: url('../../../../Assets/Logo.PNG'); width: 50px; height: 50px;">Helping Hand</h1> */}
+        <h1 class="text-hide" style="background-image: url('../../../../Assets/Logo.PNG'); width: 50px; height: 50px;">Helping Hand</h1>
         <i className="fad fa-hands-helping fa-10x"></i>
         <a className="navbar-brand" href="/">
 
 
           <ul className="list-inline">
             <li className="list-inline-item">
+            <a class="navbar-item" href="/">
+           
+                {/* <img src="Logo.PNG" alt="CaughtJA" width="50" height="50"/> */}
+               
+            </a>
               <h3>Helping Hands</h3>
             </li>
           </ul>
