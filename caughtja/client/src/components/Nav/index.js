9b5eb3ce76135537f84058fc66fc4399b2b06c5a
Tedla B <mekdi.td87@outlook.com>
2020-03-02 19:46:17 -0600

    updated the Nav

--- caughtja/client/src/components/Nav/index.js
+++ caughtja/client/src/components/Nav/index.js
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
       <a className="navbar-brand" href="/">
-        React Volunteer Work List
+      <i class="fad fa-hands-helping"></i>React Volunteer Work List
       </a>
     </nav>
   );
