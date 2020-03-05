1e5cddfcce821140b9af8df56dba010ad67c1a2f
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2020-03-05 11:54:01 -0600

    Added links and login and about page.

--- caughtja/client/src/App.js
+++ caughtja/client/src/App.js
@@ -1,22 +1,70 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Events from "./pages/Events";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
+import Login from "./pages/Login";
+import About from "./pages/About";
+
 import Nav from "./components/Nav";
+import { useState } from "react";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // useEffect(() => {
+  //   axios.get("/api/hello").then(result => {
+  //     console.log(result.data);
+  //   })
+  // }, [])
+
+
+  function signUp(e) {
+    console.log(e)
+    let input1 = document.getElementById("input1").value
+    let input2 = document.getElementById("input2").value
+    console.log(input1, input2)
+    let element = document.createElement("p")
+    let otherElement = document.getElementById("welcome")
+    element.innerHTML = `Welcome ${input1}`
+    otherElement.append(element)
+
+
+    // fetch("http://localhost:3000/api/events", {
+    //   method: "POST",
+    //   body: JSON.stringify({ "email": input1, "password": input2 })
+    // }).then(function (res) {
+    //   let element = document.createElement("p")
+    //   // element.innerHTML = res.email
+    //   console.log(res)
+    // })
+  }
+
   return (
     <Router>
-      <div>
+      <div className="App">
+        <label for="email">Email: </label>
+        <input id="input1" name="email" type="text" value={email} onChange={(event) => { setEmail(event.target.value) }} /><br />
+        <label for="email">Password: </label>
+        <input id="input2" name="password" type="text" value={password} onChange={(event) => { setPassword(event.target.value) }} /><br />
+        <button onClick={e => signUp(e)}>Sign Up!</button>
+
+        <div>
+          {email} - {password}
+        </div>
+
         <Nav />
         <Switch>
-          <Route exact path={["/", "/Events"]}>
-            <Events />
-          </Route>
-          <Route exact path="/Events/:id">
-            <Detail />
-          </Route>
+          <Route exact path="/" component={Login} />
+          {/* <Events /> */}
+          {/* </Route> */}
+          <Route exact path="/Events/" component={Events} />
+          <Route exact path="/About/" component={About} />
+
+          {/* <Detail /> */}
+          {/* </Route> */}
           <Route>
             <NoMatch />
           </Route>
--- caughtja/client/src/pages/About.js
+++ caughtja/client/src/pages/About.js
@@ -1,50 +1,89 @@
-import React from "react";
+// import React from "react";
 
-const document = "/Caughtja/caughtja/Assets/Tedla.jpg";
+// const document = "/Caughtja/caughtja/Assets/Tedla.jpg";
 
-// function About(){
+// // function About(){
 
-//     return (
-//         <div class="d-inline p-2 bg-primary text-white">
+// //     return (
+// //         <div class="d-inline p-2 bg-primary text-white">
 
-//             <img src="../Assets/Tedla.jpg" alt="Tedla Betsuan" class="rounded-circle">
-//                 Tedla Betsuan
-//             </img>
+// //             <img src="../Assets/Tedla.jpg" alt="Tedla Betsuan" class="rounded-circle">
+// //                 Tedla Betsuan
+// //             </img>
 
-//             <img src="../../../../Assets/Josh.jpg" alt="Josh K" class="rounded-circle">
-//                 Josh K
-//             </img>
+// //             <img src="../../../../Assets/Josh.jpg" alt="Josh K" class="rounded-circle">
+// //                 Josh K
+// //             </img>
 
-//             <img src="https://ca.slack-edge.com/TQ7TNT9V0-UQSKFF4UE-56aed9e3dc99-512" alt="Christopher L Garcia" class="rounded-circle">
-//                 Christopher L Garcia
-//             </img>
+// //             <img src="https://ca.slack-edge.com/TQ7TNT9V0-UQSKFF4UE-56aed9e3dc99-512" alt="Christopher L Garcia" class="rounded-circle">
+// //                 Christopher L Garcia
+// //             </img>
         
         
-//         </div>
+// //         </div>
 
         
-//     )
-// }
+// //     )
+// // }
 
-function About() {
-    if (this.state.document) {
-      const document = this.state.document;
+// function About() {
+//     if (this.state.document) {
+//       const document = this.state.document;
    
-      const mainView = document.data.responsive_image;
-      const tabletView = document.data.responsive_image.tablet;
-      const mobileView = document.data.responsive_image.mobile;
+//       const mainView = document.data.responsive_image;
+//       const tabletView = document.data.responsive_image.tablet;
+//       const mobileView = document.data.responsive_image.mobile;
    
-      return (
-        <picture>
-          <source media="(max-width: 400px)" srcSet={mobileView.url} />
-          <source media="(max-width: 900px)" srcSet={tabletView.url} />
-          <source srcSet={mainView.url} />
-          <img src={document.data.illustration.url} alt={document.data.illustration.alt} />
-        </picture>
-      );
-    } else {
-      return null;
-    }
+//       return (
+//         <picture>
+//           <source media="(max-width: 400px)" srcSet={mobileView.url} />
+//           <source media="(max-width: 900px)" srcSet={tabletView.url} />
+//           <source srcSet={mainView.url} />
+//           <img src={document.data.illustration.url} alt={document.data.illustration.alt} />
+//         </picture>
+//       );
+//     } else {
+//       return null;
+//     }
+//   }
+
+// export default About;
+
+
+
+
+
+// import { Route } from "react-router-dom";
+import React, { Component } from "react";
+// import API from "../utils/API";
+// import React, { useState, useEffect } from "react";
+// import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
+// import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+// import { List, ListItem } from "../components/List";
+// import { Input, TextArea, FormBtn } from "../components/Form";
+
+class About extends Component {
+  render(){
+    return (
+        <Container fluid>
+             <Row>
+             <Col size="md-6">
+                <Jumbotron>
+                <h1>WELCOME TO OUR PAGE</h1>
+                 </Jumbotron>
+                 <div id="welcome">
+                   
+                 <h2>BY: CHRIS, JOSH, TEDLA</h2>
+                 <p>MWHAHAHAHA</p>
+                 </div>
+                 </Col>
+                 </Row>
+                </Container>
+    )
+
   }
+}
 
 export default About;
