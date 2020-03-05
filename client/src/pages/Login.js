97149a305b42db815eecefbe3551c50db446b9c5
Joshua Kuruvilla <joshkuruvilla512@gmail.com>
2020-03-05 17:24:16 -0600

    Changed the layout of the navbar.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,4 @@
-// import { Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import React, { Component } from "react";
 // import API from "../utils/API";
 // import React, { useState, useEffect } from "react";
@@ -8,27 +8,23 @@ import Jumbotron from "../components/Jumbotron";
 import { Col, Row, Container } from "../components/Grid";
 // import { List, ListItem } from "../components/List";
 // import { Input, TextArea, FormBtn } from "../components/Form";
-
 class Login extends Component {
   render(){
     return (
-        <Container fluid>
+        <Container fluid >
              <Row>
              <Col size="md-6">
                 <Jumbotron>
                 <h1>Lend A Helping Hand</h1>
                  </Jumbotron>
                  <div id="welcome">
-                   
                  <h2>Find and Create Events Near You to Volunteer</h2>
-                 <p>Login to get view or create an event...</p>
+                 <p>Login to view or create an event...</p>
                  </div>
                  </Col>
                  </Row>
                 </Container>
     )
-
   }
 }
-
 export default Login;
